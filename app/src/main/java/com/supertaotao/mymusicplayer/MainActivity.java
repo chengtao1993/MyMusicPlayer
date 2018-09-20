d209fb0a0d12c7297e4d7738ea6a2f44aa10f58a
chengtao.he <supertaotao520@vip.qq.com>
2018-09-20 18:26:15 +0800

    add playfragment and icon resource

--- app/src/main/java/com/supertaotao/mymusicplayer/MainActivity.java
+++ app/src/main/java/com/supertaotao/mymusicplayer/MainActivity.java
@@ -1,13 +1,19 @@
 package com.supertaotao.mymusicplayer;
-
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.fragment_container,new PlayFragment());
+        mFragmentTransaction.commit();
     }
 }
